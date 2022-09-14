ddc1a2fd74368e1e169757896745c492d7e6b504
Splashc <plopkoek250@gmail.com>
2022-09-14 09:55:21 +0200

    Test

--- Mod5_oefening1/src/Oefening1.java
+++ Mod5_oefening1/src/Oefening1.java
@@ -5,7 +5,12 @@ public class Oefening1
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		
+		double begin = 21;
+		double stijg = 0.78;
+		double antwoord = begin + stijg;
+		System.out.println("Het is nu " + begin + " Graden");
+		System.out.println("Maar het wordt " + stijg + " Graden Warmer");
+		System.out.println("En dan wordt het: " + antwoord + " Graden");
 	}
 
 }
