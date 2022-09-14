367f5a1e9e426c57754da260488d2d627c4f4812
Splashc <plopkoek250@gmail.com>
2022-09-14 10:11:28 +0200

    code

---
+++ Mod5_oefening1/src/Oefening2.java
@@ -0,0 +1,14 @@
+
+public class Oefening2
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		float getal1 = 5;
+		float getal2 = 6;
+		float antwoord = getal1 + getal2;
+		System.out.println(antwoord);
+	}
+
+}
