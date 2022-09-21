58b598a4df33e9ed91eca31c6b3bc2614a9fdc60
Splashc <plopkoek250@gmail.com>
2022-09-21 10:04:50 +0200

    f

---
+++ Mod6_Oefening1/src/Oefening1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Oefening1
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Scanner Keyboard = new Scanner(System.in);
+		System.out.print("Voer heel getal in: ");
+		int inputNumber = Keyboard.nextInt();
+		System.out.println("Het ingevoerde getal is: " + inputNumber);
+	}
+
+}
